009c65a6d30a976c76ce437af54cb596a6f02a58
Anthony Leif <tonyleif@hotmail.com>
2017-10-17 21:51:44 -0500

    Page 369

---
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+    }
+}
