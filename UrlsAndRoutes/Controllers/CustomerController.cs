c60dad2f4a6fc2cb19c8e149112eb9530ecc8ee1
Anthony Leif <tonyleif@hotmail.com>
2017-10-20 21:15:04 -0500

    Page 400

--- UrlsAndRoutes/Controllers/CustomerController.cs
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -2,9 +2,10 @@
 
 namespace UrlsAndRoutes.Controllers
 {
+    [RoutePrefix("Users")]
     public class CustomerController : Controller
     {
-        [Route("Test")]
+        [Route("~/Test")]
         public ActionResult Index()
         {
             ViewBag.Controller = "Customer";
@@ -12,13 +13,13 @@ namespace UrlsAndRoutes.Controllers
             return View("ActionName");
         }
 
-        [Route("Users/Add/{user}/{id:int}")]
+        [Route("Add/{user}/{id:int}")]
         public string Create(string user, int id)
         {
             return string.Format("User: {0}, ID: {1}", user, id);
         }
 
-        [Route("Users/Add/{user}/{password}")]
+        [Route("Add/{user}/{password}")]
         public string ChangePass(string user, string password)
         {
             return string.Format("ChangePass Method - User: {0}, Pass: {1}", user, password);
