ac5e39fecc7e1fc210fb47410d2c389e935e7adb
Anthony Leif <tonyleif@hotmail.com>
2017-10-24 20:52:37 -0500

    Page 408

--- UrlsAndRoutes.Tests/RouteTests.cs
+++ UrlsAndRoutes.Tests/RouteTests.cs
@@ -73,20 +73,20 @@ namespace UrlsAndRoutes.Tests
             Assert.IsTrue(result == null || result.Route == null);
         }
 
-        [TestMethod]
-        public void TestIncomingRoutes()
-        {
-            TestRouteMatch("∼/", "Home", "Index");
-            TestRouteMatch("∼/Home", "Home", "Index");
-            TestRouteMatch("∼/Home/Index", "Home", "Index");
-            TestRouteMatch("∼/Home/About", "Home", "About");
-            TestRouteMatch("∼/Home/About/MyId", "Home", "About", new { id = "MyId" });
-            TestRouteMatch("∼/Home/About/MyId/More/Segments", "Home", "About",
-            new { id = "MyId", catchall = "More/Segments" });
-            TestRouteFail("∼/Home/OtherAction");
-            TestRouteFail("∼/Account/Index");
-            TestRouteFail("∼/Account/About");
-        }
+        //[TestMethod]
+        //public void TestIncomingRoutes()
+        //{
+        //    TestRouteMatch("∼/", "Home", "Index");
+        //    TestRouteMatch("∼/Home", "Home", "Index");
+        //    TestRouteMatch("∼/Home/Index", "Home", "Index");
+        //    TestRouteMatch("∼/Home/About", "Home", "About");
+        //    TestRouteMatch("∼/Home/About/MyId", "Home", "About", new { id = "MyId" });
+        //    TestRouteMatch("∼/Home/About/MyId/More/Segments", "Home", "About",
+        //    new { id = "MyId", catchall = "More/Segments" });
+        //    TestRouteFail("∼/Home/OtherAction");
+        //    TestRouteFail("∼/Account/Index");
+        //    TestRouteFail("∼/Account/About");
+        //}
     }
 }
 
--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace UrlsAndRoutes
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.MapMvcAttributeRoutes();
-            routes.MapRoute("Default", "{controller}/{action}/{id}",
-                new {controller = "Home", action = "Index", id = UrlParameter.Optional },
-                new[] { "UrlsAndRoutes.Controllers" });
+
+            //routes.MapRoute("NewRoute", "App/Do{action}", new { controller = "Home" });
+
+            routes.MapRoute("MyRoute", "{controller}/{action}/{id}", 
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+            
+            //routes.MapRoute("Default", "{controller}/{action}/{id}",
+            //    new {controller = "Home", action = "Index", id = UrlParameter.Optional },
+            //    new[] { "UrlsAndRoutes.Controllers" });
 
             //    routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
