d27c72825802f463c9d1e8b7aba669d33da8614e
Anthony Leif <tonyleif@hotmail.com>
2017-10-25 22:39:27 -0500

    Page 418

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -14,6 +14,9 @@ namespace UrlsAndRoutes
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.MapMvcAttributeRoutes();
+
+            routes.Add(new LegacyRoute("∼/articles/Windows_3.1_Overview.html","∼/old/.NET_1.0_Class_Library"));
+        
             routes.MapRoute("MyRoute", "{controller}/{action}");
             routes.MapRoute("MyOtherRoute", "App/{action}", new { controller = "Home" });
 
