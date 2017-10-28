8fe60c30e048141c47f0b44669240440edfbdda9
Anthony Leif <tonyleif@hotmail.com>
2017-10-27 20:59:47 -0500

    Page 433

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -13,7 +13,12 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.RouteExistingFiles = true;
             routes.MapMvcAttributeRoutes();
+
+            routes.IgnoreRoute("Content/{filename}.html");
+
+            //routes.MapRoute("DiskFile", "Content/StaticContent.html", new { controller = "Customer", action = "List" });
 
             routes.Add(new Route("SayHello", new CustomRouteHandler()));
             routes.Add(new LegacyRoute("∼/articles/Windows_3.1_Overview.html","∼/old/.NET_1.0_Class_Library"));
